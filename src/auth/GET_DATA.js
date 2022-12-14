95dde3bc223c705655dac316d6eea16263897261
Matt <maty.britez7@gmail.com>
2022-12-14 20:34:03 -0300

    change url

--- src/auth/GET_DATA.js
+++ src/auth/GET_DATA.js
@@ -4,7 +4,7 @@ const GET_DATA = async (token) => {
   const config = {
     headers: { Authorization: `Bearer ${token}` },
   };
-  const res = await axios.get("http://localhost:4000/users/me", config);
+  const res = await axios.get("https://aleft.onrender.com/users/me", config);
   const data = res.data;
   console.log(data)
   return data;
